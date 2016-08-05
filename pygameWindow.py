dbfa8b9b1f4067a333f0d1e2e0cedec60da70131
humansnake <ashleydlatimer@gmail.com>
2016-08-05 21:06:55 +0100

    window fix

--- pygameWindow.py
+++ pygameWindow.py
@@ -2,8 +2,7 @@ import pygame
 
 pygame.init()
 gameDisplay = pygame.display.set_mode((800,600))
-pygame.display.set_caption('Hello World!')
-clock = pygame.time.clock()
+pygame.display.set_caption('Chess')
 
 closed = False
 
@@ -13,9 +12,6 @@ while not closed:
                 closed = True
 
                 print (event)
-
-            pygame.display.update()
-            clock.tick(60)
 
 pygame.quit()
 quit()
